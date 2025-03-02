bfbc5dc542b56900c3c25244e21308f01c48ec55
Sushant Bhandari <133732265+SushantBhn@users.noreply.github.com>
2025-03-02 17:14:15 +0545

    Add initial calculator layout
    Also added a general function to perform basic operations

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let operand1, operator, operand2;
+
 function add(a, b) {
     return a + b;
 }
@@ -12,4 +14,24 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function operate(operand1, operator, operand2) {
+    switch(operator) {
+        case "+":
+            return add(operand1, operand2);
+            break;
+
+        case "-":
+            return subtract(operand1, operand2);
+            break;
+
+        case "*":
+            return multiply(operand1, operand2);
+            break;
+
+        case "/":
+            return divide(operand1, operand2);
+            break;
+    }
 }
